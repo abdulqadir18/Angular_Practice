5bff9ce830d2a70da0be38b0637355b94f93a422
abdulqadir18 <boxwalaabdulqadir2002@gmail.com>
2023-06-08 09:35:39 +0530

    created first app in angular

---
+++ first-app/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,18 @@
+import { Component , Input} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housing-location';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      housing-location works!
+    </p>
+  `,
+  styleUrls: ['./housing-location.component.css']
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+}
